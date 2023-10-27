30d67cbeb89dd0feae2c3cb90051015dc99a4246
carla paloma <cpcodes@pm.me>
2023-10-27 07:54:02 -0700

    h4 portable text my

--- app/(site)/components/global/RichTextComponents.tsx
+++ app/(site)/components/global/RichTextComponents.tsx
@@ -53,7 +53,7 @@ export const RichTextComponents = {
       h1: ({children} : any) => <h1 className="text-4xl lg:text-6xl py-2 mb-3 uppercase font-mono text-yellow-600">{children}</h1>,
       h2: ({children} : any) => <h1 className="text-3xl  lg:text-5xl py-2 mb-3 uppercase font-mono text-yellow-600">{children}</h1>,
       h3: ({children} : any) => <h1 className="text-2xl  lg:text-4xl py-2 mb-3 uppercase font-mono text-yellow-600">{children}</h1>,
-      h4: ({children} : any) => <h1 className="text-xl  lg:text-3xl py-2 mt-3 mb-3 uppercase font-mono text-yellow-600">{children}</h1>,
+      h4: ({children} : any) => <h1 className="text-xl  lg:text-3xl py-2 my-3 uppercase font-mono text-yellow-600">{children}</h1>,
 
       blockquote: ({children} : any) => <blockquote className="border-l-yellow-500">{children}</blockquote>,
   }
