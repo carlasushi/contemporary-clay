49c274304dd36197e1c6765b164c4f8737b9bbb0
carla paloma <cpcodes@pm.me>
2023-10-29 22:38:10 -0700

    teal

--- app/(site)/components/global/RichTextComponents.tsx
+++ app/(site)/components/global/RichTextComponents.tsx
@@ -68,10 +68,10 @@ export const RichTextComponents = {
 
     block: {
       // Ex. 1: customizing common block types
-      h1: ({children} : any) => <h1 className="text-4xl lg:text-6xl py-2 my-3 uppercase font-mono text-teal-950">{children}</h1>,
-      h2: ({children} : any) => <h1 className="text-3xl  lg:text-5xl py-2 my-3 uppercase font-mono text-teal-950">{children}</h1>,
-      h3: ({children} : any) => <h1 className="text-2xl  lg:text-4xl py-2 my-3 uppercase font-mono text-teal-950">{children}</h1>,
-      h4: ({children} : any) => <h1 className="text-xl  lg:text-3xl py-2 my-3 uppercase font-mono text-teal-950">{children}</h1>,
+      h1: ({children} : any) => <h1 className="text-4xl lg:text-6xl py-2 my-3 uppercase font-mono text-teal-600">{children}</h1>,
+      h2: ({children} : any) => <h1 className="text-3xl  lg:text-5xl py-2 my-3 uppercase font-mono text-teal-600">{children}</h1>,
+      h3: ({children} : any) => <h1 className="text-2xl  lg:text-4xl py-2 my-3 uppercase font-mono text-teal-600">{children}</h1>,
+      h4: ({children} : any) => <h1 className="text-xl  lg:text-3xl py-2 my-3 uppercase font-mono text-teal-600">{children}</h1>,
 
       blockquote: ({children} : any) => <blockquote className="border-l-yellow-500">{children}</blockquote>,
   }
